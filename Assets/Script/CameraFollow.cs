2eb030da854eba491c33557b6534b243235effe8
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-08-18 23:03:04 +0700

    Merge Warp and overall

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -8,6 +8,7 @@ public class CameraFollow : MonoBehaviour
     public Transform player;
     void Update()
     {
-        transform.position = new Vector3(0, player.position.y,-10);
+            transform.position = new Vector3(0, player.position.y,-10);
+        
     }
 }
