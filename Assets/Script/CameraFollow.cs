1c5e3af4f0173a7c3339ba086ac154b935d224c0
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2023-08-18 22:50:09 +0700

    New Camera

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    void Update()
+    {
+        transform.position = new Vector3(0, player.position.y,-10);
+    }
+}
